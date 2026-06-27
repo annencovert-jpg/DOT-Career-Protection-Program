9689ad94d8a07885dc60c3a048daa8549f8523bc
annencovert-jpg <annencovert@gmail.com>
2026-06-27 16:42:42 -0500

    Add React Vite website preview

---
+++ website/vite.config.ts
@@ -0,0 +1,8 @@
+declare const process: { env: { GITHUB_REPOSITORY?: string } };
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  plugins: [react()],
+});
